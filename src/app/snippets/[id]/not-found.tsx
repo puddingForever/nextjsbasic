02517f00a6071802796dae69c69d905581fbd918
BytePudding <developerjieun94@gmail.com>
2025-01-04 16:53:00 +0900

    Initial commit

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,5 @@
+export default function SnippetNotFound(){
+    return <div>
+        <h1> NOT FOUND</h1>
+    </div>
+}
---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,52 @@
+import {redirect} from 'next/navigation';
+import {db} from '@/db';
+
+export default function SnippetCreatePage(){
+
+    async function createSnippet(formData : FormData){
+            // server action 
+            'use server'
+            // user input (make sure its valid)
+            const title = formData.get('title') as string;
+            const code = formData.get('code') as string;
+
+            // create a new record 
+            const snippet =  await db.snippet.create({
+                data:{
+                    title,
+                    code
+                }
+            })
+          
+            // redirect the user to the root 
+            redirect("/");
+    }
+
+
+
+
+    return <form action={createSnippet}>
+            <h3 className="font-bold m-3">Create a Snippet</h3>
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-4">
+                    <label className="w-12" htmlFor="title">
+                        Title
+                    </label>
+                    <input name="title" className="border rounded p-2 w-full"
+                    id="title" />
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-4">
+                    <label className="w-12" htmlFor="code">
+                        Code
+                    </label>
+                    <textarea name="code" className="border rounded p-2 w-full"
+                    id="code" />
+                </div>
+                <button type="submit" className="rounded p-2 bg-blue-200">
+                    Create
+                </button>
+            </div>
+        </form>
+}   
